97b97221fb11da424a5fd1df097a0704a8291e2a
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-01-04 23:02:41 +0300

    Add Console Demo Css & Html

---
+++ demo/Skylark.Console.Demo/ConsoleDemoCss/ConsoleDemoCss/Program.cs
@@ -0,0 +1,20 @@
+﻿using Skylark.Extension;
+
+namespace ConsoleDemoCss
+{
+    internal class Program
+    {
+        private const string Css = "@charset 'UTF-8';\n* {\r\n\tmargin: 0;\r\n\tpadding: 0;\r\n\t-webkit-text-size-adjust: none;\r\n}";
+
+        static void Main()
+        {
+            string Minify = CssExtension.ToMinify(Css);
+            Console.WriteLine(Minify);
+
+            Console.WriteLine();
+
+            string Beauty = CssExtension.ToBeauty(Minify);
+            Console.WriteLine(Beauty);
+        }
+    }
+}
---
+++ demo/Skylark.Console.Demo/ConsoleDemoHtml/ConsoleDemoHtml/Program.cs
@@ -0,0 +1,28 @@
+﻿using Skylark.Extension;
+
+namespace ConsoleDemoHtml
+{
+    internal class Program
+    {
+        private const string Html = "<html>\n\t<head>\n\t\t<title>Taiizor Skylark</title>\n\t</head>\n</html>";
+
+        static void Main()
+        {
+            string Encode = HtmlExtension.Encode(Html);
+            Console.WriteLine(Encode);
+
+            string Decode = HtmlExtension.Decode(Encode);
+            Console.WriteLine(Decode);
+
+            Console.WriteLine();
+
+            string Minify = HtmlExtension.ToMinify(Html);
+            Console.WriteLine(Minify);
+
+            Console.WriteLine();
+
+            string Beauty = HtmlExtension.ToBeauty(Minify);
+            Console.WriteLine(Beauty);
+        }
+    }
+}
