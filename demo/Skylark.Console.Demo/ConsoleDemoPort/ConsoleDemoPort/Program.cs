960e549e18917bd743576441e2490710d9f6ba63
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-01-05 00:16:02 +0300

    Add Console Demo Port

---
+++ demo/Skylark.Console.Demo/ConsoleDemoPort/ConsoleDemoPort/Program.cs
@@ -0,0 +1,35 @@
+﻿using Skylark.Enum;
+using Skylark.Extension;
+
+namespace ConsoleDemoPort
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Info1 = PortExtension.Info(443);
+            Console.WriteLine(Info1);
+
+            Console.WriteLine();
+
+            Dictionary<int, string> Info2 = PortExtension.Info(new[] { 21, 22, 53, 80, 110, 143, 587, 3306 });
+            foreach (KeyValuePair<int, string> Pair in Info2)
+            {
+                Console.WriteLine($"{Pair.Key}: {Pair.Value}");
+            }
+
+            Console.WriteLine();
+
+            PortType Scan1 = PortExtension.Scan("www.google.com", 443, 1000);
+            Console.WriteLine(Scan1);
+
+            Console.WriteLine();
+
+            Dictionary<int, PortType> Scan2 = PortExtension.Scan("www.google.com", new[] { 21, 80, 443 }, 500);
+            foreach (KeyValuePair<int, PortType> Pair in Scan2)
+            {
+                Console.WriteLine($"{Pair.Key}: {Pair.Value}");
+            }
+        }
+    }
+}
