5d4135373861d5e30f03cccb5de0df8dd47e13d5
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-01-04 23:47:19 +0300

    Add Console Demo Lottery

---
+++ demo/Skylark.Console.Demo/ConsoleDemoLottery/ConsoleDemoLottery/Program.cs
@@ -0,0 +1,33 @@
+﻿using Skylark.Extension;
+using Skylark.Struct;
+
+namespace ConsoleDemoLottery
+{
+    internal class Program
+    {
+        private const string List = "Taiizor\nReaLTaiizor\nSkylark\nConforyon\nStarterkit\nReaLTaiizor\nTaiizor";
+
+        static void Main()
+        {
+            LotteryDrawStruct Draw = LotteryExtension.Draw(List, 2, 3, false);
+            foreach (string Winner in Draw.Winners)
+            {
+                Console.WriteLine($"Winner {Draw.Winners.IndexOf(Winner) + 1}: {Winner}");
+            }
+            foreach (string Reserve in Draw.Reserve)
+            {
+                Console.WriteLine($"Reserve {Draw.Reserve.IndexOf(Reserve) + 1}: {Reserve}");
+            }
+
+            Console.WriteLine();
+
+            string Suffle1 = LotteryExtension.Suffle(List, false);
+            Console.WriteLine(Suffle1);
+
+            Console.WriteLine("=====^^=====");
+
+            string Suffle2 = LotteryExtension.Suffle(List, true);
+            Console.WriteLine(Suffle2);
+        }
+    }
+}
