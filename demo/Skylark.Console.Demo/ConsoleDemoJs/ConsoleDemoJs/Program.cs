ba838557fb248597d007437b9f9089aecf8f6f03
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-01-04 23:07:20 +0300

    Add Console Demo Js

---
+++ demo/Skylark.Console.Demo/ConsoleDemoJs/ConsoleDemoJs/Program.cs
@@ -0,0 +1,20 @@
+﻿using Skylark.Extension;
+
+namespace ConsoleDemoJs
+{
+    internal class Program
+    {
+        private const string Js = "Taiizor = function(message = 'Skylark')\r\n{\r\n\tconsole.log(message);\r\n}";
+
+        static void Main()
+        {
+            string Minify = JsExtension.ToMinify(Js);
+            Console.WriteLine(Minify);
+
+            Console.WriteLine();
+
+            string Beauty = JsExtension.ToBeauty(Minify);
+            Console.WriteLine(Beauty);
+        }
+    }
+}
