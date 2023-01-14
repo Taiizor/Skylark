1e77c0461854d6f792b8c46e1ab9de7f1339149f
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-01-14 16:54:42 +0300

    Infrastructure Preparation 24

---
+++ src/Skylark/Helper/Board.cs
@@ -0,0 +1,244 @@
+﻿using E = Skylark.Exception;
+using MS = Skylark.Manage.Special;
+
+namespace Skylark.Helper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Board
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Byte"></param>
+        /// <exception cref="E"></exception>
+        public static void Copy(byte[] Byte)
+        {
+            try
+            {
+                MS.BoardBytes = Byte;
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="String"></param>
+        /// <exception cref="E"></exception>
+        public static void Copy(string String)
+        {
+            try
+            {
+                MS.BoardText = String;
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Integer"></param>
+        /// <exception cref="E"></exception>
+        public static void Copy(int Integer)
+        {
+            try
+            {
+                MS.BoardNumber = Integer;
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Boolean"></param>
+        /// <exception cref="E"></exception>
+        public static void Copy(bool Boolean)
+        {
+            try
+            {
+                MS.BoardBool = Boolean;
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Clear"></param>
+        /// <param name="Back"></param>
+        /// <returns></returns>
+        /// <exception cref="E"></exception>
+        public static byte[] Paste(bool Clear = false, byte[] Back = null)
+        {
+            try
+            {
+                byte[] Byte = MS.BoardBytes;
+
+                if (Clear)
+                {
+                    MS.BoardBytes = Array.Empty<byte>();
+                }
+
+                if (Byte == null)
+                {
+                    return Back;
+                }
+                else
+                {
+                    return Byte;
+                }
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Clear"></param>
+        /// <param name="Back"></param>
+        /// <returns></returns>
+        /// <exception cref="E"></exception>
+        public static Stream Paste(bool Clear = false, Stream Back = null)
+        {
+            try
+            {
+                Stream Stream = new MemoryStream(MS.BoardBytes);
+
+                if (Clear)
+                {
+                    MS.BoardBytes = Array.Empty<byte>();
+                }
+
+                if (Stream == null)
+                {
+                    return Back;
+                }
+                else
+                {
+                    return Stream;
+                }
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Clear"></param>
+        /// <param name="Back"></param>
+        /// <returns></returns>
+        /// <exception cref="E"></exception>
+        public static string Paste(bool Clear = false, string Back = "Empty!")
+        {
+            try
+            {
+                string String = MS.BoardText;
+
+                if (Clear)
+                {
+                    MS.BoardText = string.Empty;
+                }
+
+                if (string.IsNullOrEmpty(String))
+                {
+                    return Back;
+                }
+                else
+                {
+                    return String;
+                }
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Clear"></param>
+        /// <param name="Back"></param>
+        /// <returns></returns>
+        /// <exception cref="E"></exception>
+        public static int Paste(bool Clear = false, int Back = 0)
+        {
+            try
+            {
+                int Integer = MS.BoardNumber;
+
+                if (Clear)
+                {
+                    MS.BoardNumber = 0;
+                }
+
+                if (Integer > 0)
+                {
+                    return Back;
+                }
+                else
+                {
+                    return Integer;
+                }
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Clear"></param>
+        /// <param name="Back"></param>
+        /// <returns></returns>
+        /// <exception cref="E"></exception>
+        public static bool Paste(bool Clear = false, bool Back = false)
+        {
+            try
+            {
+                bool Boolean = MS.BoardBool;
+
+                if (Clear)
+                {
+                    MS.BoardBool = false;
+                }
+
+                if (Boolean != true)
+                {
+                    return Back;
+                }
+                else
+                {
+                    return Boolean;
+                }
+            }
+            catch (E Ex)
+            {
+                throw new E(Ex.Message, Ex);
+            }
+        }
+    }
+}
---
+++ src/Skylark/Helper/Culture.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Skylark.Helper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Culture
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CultureInfo Current
+        {
+            get => CultureInfo.CurrentCulture;
+            set => CultureInfo.CurrentCulture = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CultureInfo CurrentUI
+        {
+            get => CultureInfo.CurrentUICulture;
+            set => CultureInfo.CurrentUICulture = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CultureInfo ThreadCurrent
+        {
+            get => CultureInfo.DefaultThreadCurrentCulture;
+            set => CultureInfo.DefaultThreadCurrentCulture = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CultureInfo ThreadCurrentUI
+        {
+            get => CultureInfo.DefaultThreadCurrentCulture;
+            set => CultureInfo.DefaultThreadCurrentUICulture = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CultureInfo Invariant => CultureInfo.InvariantCulture;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CultureInfo InstalledUI => CultureInfo.InstalledUICulture;
+    }
+}
