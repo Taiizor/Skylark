a8b67712a6585e34a68211342d8b0a59536bc005
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-03-09 14:49:35 +0300

    Infrastructure Preparation 151

--- src/Skylark/Skylark.cs
+++ src/Skylark/Skylark.cs
@@ -12,8 +12,8 @@ using SE = System.Exception;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 03.Jan.2023
-//     Changed: 02.Mar.2023
-//     Version: 3.0.0.4
+//     Changed: 09.Mar.2023
+//     Version: 3.0.0.5
 //
 // |---------DO-NOT-REMOVE---------|
 
