b1c540d3683f49513d31e8d3f652c5cd9fc488c3
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-03-15 15:45:11 +0300

    Update Skylark.cs

--- src/Skylark/Skylark.cs
+++ src/Skylark/Skylark.cs
@@ -12,7 +12,7 @@ using SE = System.Exception;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 03.Jan.2023
-//     Changed: 09.Mar.2023
+//     Changed: 15.Mar.2023
 //     Version: 3.0.0.5
 //
 // |---------DO-NOT-REMOVE---------|
