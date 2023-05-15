f07482a60784948429b23b1d040b177dd6059fa1
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-05-15 18:24:49 +0300

    Infrastructure Preparation 157

--- src/Skylark.Uptime/Skylark.Uptime.cs
+++ src/Skylark.Uptime/Skylark.Uptime.cs
@@ -17,8 +17,8 @@ using SUMI = Skylark.Uptime.Manage.Internal;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 27.Feb.2023
-//     Changed: 23.Mar.2023
-//     Version: 3.0.0.6
+//     Changed: 15.May.2023
+//     Version: 3.0.0.7
 //
 // |---------DO-NOT-REMOVE---------|
 
