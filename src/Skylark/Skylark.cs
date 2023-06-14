81ac540230cdb53747f9caa7cc37dbff1052ef6b
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-06-14 13:48:40 +0300

    Update SDK

--- src/Skylark/Skylark.cs
+++ src/Skylark/Skylark.cs
@@ -12,7 +12,7 @@ using SE = System.Exception;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 03.Jan.2023
-//     Changed: 06.Jun.2023
+//     Changed: 14.Jun.2023
 //     Version: 3.0.1.0
 //
 // |---------DO-NOT-REMOVE---------|
