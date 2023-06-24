f99a71065f0cd9ef69081995c6ccfe3f6c3ef056
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-06-24 20:42:30 +0300

    Infrastructure Preparation 186

--- src/Skylark.Uptime/Skylark.Uptime.cs
+++ src/Skylark.Uptime/Skylark.Uptime.cs
@@ -18,7 +18,7 @@ using SUMI = Skylark.Uptime.Manage.Internal;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 27.Feb.2023
-//     Changed: 23.Jun.2023
+//     Changed: 24.Jun.2023
 //     Version: 3.0.1.4
 //
 // |---------DO-NOT-REMOVE---------|
---
+++ src/Skylark/Enum/ThemeType.cs
@@ -0,0 +1,40 @@
+﻿namespace Skylark.Enum
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum ThemeType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Auto,
+        /// <summary>
+        /// 
+        /// </summary>
+        Dark,
+        /// <summary>
+        /// 
+        /// </summary>
+        Light,
+        /// <summary>
+        /// 
+        /// </summary>
+        Custom
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum WindowsThemeType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Dark,
+        /// <summary>
+        /// 
+        /// </summary>
+        Light
+    }
+}
