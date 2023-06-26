f54bb4decd915299014f904b7b381dffff454efe
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-06-26 03:56:09 +0300

    Infrastructure Preparation 189

--- src/Skylark/Skylark.cs
+++ src/Skylark/Skylark.cs
@@ -12,8 +12,8 @@ using SE = System.Exception;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 03.Jan.2023
-//     Changed: 25.Jun.2023
-//     Version: 3.0.1.303
+//     Changed: 26.Jun.2023
+//     Version: 3.0.1.304
 //
 // |---------DO-NOT-REMOVE---------|
 
---
+++ src/Skylark/Struct/Mouse/MouseStruct.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Skylark.Struct.Mouse
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MousePointStruct
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int X;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Y;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MouseHookStruct
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MousePointStruct pt;
+        /// <summary>
+        /// 
+        /// </summary>
+        public IntPtr hwnd;
+        /// <summary>
+        /// 
+        /// </summary>
+        public uint wHitTestCode;
+        /// <summary>
+        /// 
+        /// </summary>
+        public IntPtr dwExtraInfo;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MouseExtraHookStruct
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MousePointStruct Point;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MouseData;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Flags;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Time;
+        /// <summary>
+        /// 
+        /// </summary>
+        public IntPtr ExtraInfo;
+    }
+}
