54e7281028b2014d29872782dde404cb2b7c1206
Taiizor <41683699+Taiizor@users.noreply.github.com>
2025-10-14 17:16:58 +0300

    Bump version to 3.1.9.2
    Updated version number in both source and project files to 3.1.9.2 to reflect the latest changes.

--- src/Skylark.Wing/Skylark.Wing.cs
+++ src/Skylark.Wing/Skylark.Wing.cs
@@ -22,7 +22,7 @@ using SWUS = Skylark.Wing.Utility.Screene;
 //     Website: www.vegalya.com
 //     Created: 17.Jun.2023
 //     Changed: 14.Oct.2025
-//     Version: 3.1.9.1
+//     Version: 3.1.9.2
 //
 // |---------DO-NOT-REMOVE---------|
 
