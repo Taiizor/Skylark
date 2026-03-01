edd3eeb5d826642205c2caa2e5f0d3409e5d9f0f
Taiizor <41683699+Taiizor@users.noreply.github.com>
2026-03-01 14:30:50 +0300

    Bump SourceLink and package versions
    Update Microsoft.SourceLink.GitHub to 11.0.100-preview.1.26104.118 across multiple projects; bump System.Management versions in Skylark.Wing (net9 -> 9.0.13, net10 -> 10.0.3). Add a dotnet11 package source to NuGet.Config (note: URL matches dotnet10 entry in diff). Also update 'Changed' timestamps to 01.Mar.2026 in Skylark, Skylark.Wing and Skylark.Uptime.

--- src/Skylark.Uptime/Skylark.Uptime.cs
+++ src/Skylark.Uptime/Skylark.Uptime.cs
@@ -15,7 +15,7 @@ using SSPPSS = Skylark.Struct.Ping.PingSendStruct;
 //     Creator: Taiizor
 //     Website: www.vegalya.com
 //     Created: 27.Feb.2023
-//     Changed: 05.Feb.2026
+//     Changed: 01.Mar.2026
 //     Version: 3.1.6.5
 //
 // |---------DO-NOT-REMOVE---------|
